6bf519f67749be58eb053b214ccfec08bd396167
nishi240masa <masa2401106@gmail.com>
2025-01-09 16:37:27 +0900

    add:lectureのapiを仮データが返されるようにした

--- routes/lecture.py
+++ routes/lecture.py
@@ -4,35 +4,76 @@ from models import get_days, get_times, get_classes_by_day_and_time
 lecture_bp = Blueprint('main', __name__)
 select_classes = []
 
-@lecture_bp.route('/', methods=['GET', 'POST'])
-def index():
-    global select_classes
-    days = get_days()
-    times = get_times()
-    selected_day = request.form.get('day')
-    selected_time = request.form.get('time')
-    classes = []
+@lecture_bp.route('/lectures', methods=['GET', 'POST'])
 
-    if selected_day and selected_time:
-        classes = get_classes_by_day_and_time(selected_day, int(selected_time))
+
+def index():
+    
+    
+    if request.method == 'POST':
+        # json形式で受け取る
+        data = request.json
+        
+        res = {
+            "status": "success",
+        }
+        
+        if res.get('status') == 'success':
+            return redirect(url_for('main.lecture'))
+        else:
+            return redirect(url_for('main.index'))
+    
+    lecture_all = [
+        {
+            "lecture_title": "情報システム演習",
+            "weekday": "月曜日",
+            "timetable": "1限"
+        },
+        {
+            "lecture_title": "情報システム演習",
+            "weekday": "月曜日",
+            "timetable": "2限"
+        }
+    ]
+    
 
     return render_template(
         'lecture.html',
-        days=days,
-        times=times,
-        selected_day=selected_day,
-        selected_time=selected_time,
-        classes=classes,
-        select_classes=select_classes
+        data = lecture_all,
+        
     )
 
-
-@blueprint.route('/select_class', methods=['POST'])
-def select_class():
-    global select_classes
-    select_class = request.form.get('lecture')
-
-    if select_class and select_class not in select_classes:
-        select_classes.append(select_class)
-
-    return redirect(url_for('main.index'))  # フォームをリダイレクトしてリスト更新
+def userLeacture():
+    
+    if request.method == 'POST':
+        # json形式で受け取る
+        data = request.json
+        
+        res = {
+            "status": "success",
+        }
+        
+        if res.get('status') == 'success':
+            return redirect(url_for('main.lecture'))
+        else:
+            return redirect(url_for('main.index'))
+        
+    
+    user_lecture = [
+        {
+            "lecture_id": 1,
+            "lecture_title": "情報システム演習",
+            "weekday": "月曜日",
+            "timetable": "1限",
+            "absence_count": 1
+        },
+        {
+            "lecture_id": 2,
+            "lecture_title": "情報システム演習",
+            "weekday": "月曜日",
+            "timetable": "2限",
+            "absence_count": 2
+        } 
+    ]
+    
+    return redirect(url_for('absence.home', item=user_lecture))
