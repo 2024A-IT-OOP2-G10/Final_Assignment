781149c2fb2fda3a41445c9384c3771d5b3d73ba
nishi240masa <masa2401106@gmail.com>
2025-01-21 14:49:57 +0900

    fix:import

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,7 @@ from .user import user_bp
 from .absence import absence_bp
 from .home import home_bp
 from .lecture_result import lectureResult_bp
+from .id import id_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [id_bp, home_bp, lecture_bp,lectureResult_bp]
